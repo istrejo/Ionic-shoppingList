a03745002d8b19cd831ba3494df90b8920da771a
istrejo <istrejo2106@gmail.com>
2023-02-05 19:03:19 -0400

    tab-bar

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  @ViewChild(IonTabs) tabs: IonTabs;
+  selected: string = 'tab1';
 
   constructor() {}
 
+  setSelectedTab() {
+    this.selected = this.tabs.getSelected();
+    console.log(this.selected);
+  }
 }
